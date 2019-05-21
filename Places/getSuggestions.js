cf8e31f07620b0da5e4309e817c252366aaa9a70
Yeabsera Genene <yabsmart5@gmail.com>
2019-05-21 12:41:40 +0300

    Updated

--- Places/getSuggestions.js
+++ Places/getSuggestions.js
@@ -12,9 +12,77 @@ function getSuggestion(fetchedString) {
     var matched = null
     var regex = null
     var pattern = null
+    var checkComma = null
+    var fetchedArray = null
+    var checkMajor = false
+    var checkMinor = false
+    var sameStringCheck = []
+    var majorOrMinor = null
+    var placeToBePushed = null
     for (let i = 0; i < Places.length; ++i) {
-        pattern = "^" + fetchedString
+        checkMajor = false
+        checkMinor = false
+        checkComma = fetchedString.search(',')
+        if (checkComma > 0) {
+            fetchedArray = fetchedString.split(',')
+            if (fetchedArray.length != 2) {
+                return {
+                    status: 400,
+                    message: "Enter the city and the sub city"
+                }
+            }
+            
+            if (fetchedArray[0] == " " || fetchedArray[1] == " " || fetchedArray[0] == "" || fetchedArray[1] == "") {
+                return {
+                    status: 400,
+                    message: "Enter the city and the sub city"
+                }
+            }
+
+            pattern = fetchedArray[0]
+            if (fetchedArray[0].length < 2) {
+                pattern = "^" + fetchedArray
+                regex = new RegExp(pattern, "ig")
+                matched = Places[i].minor.search(regex)
+            }
+            regex = new RegExp(pattern, "i")
+            matched = Places[i].major.search(regex)
+            if (matched >= 0) {
+                checkMajor = true
+            }
+            if (!checkMajor) {
+                matched = Places[i].minor.search(regex)
+                if (matched >= 0 && placesReturned.indexOf(Places[i].minor) === -1 && pattern != " ") {
+                    checkMinor = true
+                }
+            }
+            
+            pattern = fetchedArray[1]
+
+            pattern = fetchedArray[1]
+            if (fetchedArray[1].length < 2) {
+                pattern = "^" + fetchedArray
+            }
+            regex = new RegExp(pattern, "i")
+            if (!checkMajor) {
+                matched = Places[i].major.search(regex)
+                if (matched < 0) {
+                    checkMajor = true
+                }
+            }
+
+            if (checkMajor && checkMinor) {
+                placesReturned.push(Places[i].minor)
+            }
+        }
+        else if (fetchedString.length > 4) {
+            pattern = "" + fetchedString
+        } else if (fetchedString.length < 4) {
+            pattern = "^" + fetchedString
+        }
         regex = new RegExp(pattern, "i")
+        matched = Places[i].minor.search(regex)        
+        pattern = "^" + fetchedString
         matched = Places[i].minor.search(regex)
         if (matched >= 0) {
             placesReturned.push(Places[i].minor)
@@ -27,5 +95,13 @@ function getSuggestion(fetchedString) {
     }
 
 }
+
+
+function test() {
+    const response = getSuggestion("bole, add")
+    console.log(response)
+}
+
+//test()
 
 module.exports = getSuggestion
