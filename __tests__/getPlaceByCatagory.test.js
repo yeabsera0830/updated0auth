9e8189a3659bdcf2a68aea599d9472d6f071959b
Yeabsera Genene <yabsmart5@gmail.com>
2019-05-24 02:49:23 +0300

    Updated

--- __tests__/getPlaceByCatagory.test.js
+++ __tests__/getPlaceByCatagory.test.js
@@ -3,6 +3,5 @@ jest.setTimeout(30000)
 
 it('should ', async () => {
     const response = await getPlaceByCatagory(8.990569, 38.801838, "bar")
-    console.log(response)
     expect(response.places[0].placeID).not.toBeNull()
 });
